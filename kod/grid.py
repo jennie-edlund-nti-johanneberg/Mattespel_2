56c9120ef0649a5d4374db9d31f8c8e342e78abd
jennie-edlund-nti-johanneberg <74712753+jennie-edlund-nti-johanneberg@users.noreply.github.com>
2021-10-21 19:00:59 +0200

    fixa grid-filen så den fck mindre kod

--- kod/grid.py
+++ kod/grid.py
@@ -1,89 +1,63 @@
 #Här är ett funktionellt grid du kan tacka mig senare
 import turtle
 
+def pen(trtl, posx, posy, color):
+    trtl.penup()
+    trtl.color(color)
+    trtl.setpos(posx, posy)
+    trtl.pendown()
+
+
+#Gör själva griden
 def grid():
+    #Skapar sköldpadda + hastigheten på den
     trtl = turtle.Turtle()
-
     trtl.speed(0)
-    trtl.color("lightgrey")
 
+    #x-linjerna
     for i in range(11):
-        trtl.pu()
-        trtl.setx(-250)
-        trtl.sety(250-(50*i))
-        trtl.pd() 
+        pen(trtl, -250, 250-(50*i), "lightgrey")
         trtl.fd(500)
 
     trtl.lt(90)
 
+    #y-linjerna
     for i in range(11):
-        trtl.pu()
-        trtl.setx(250-(50*i))
-        trtl.sety(-250)
-        trtl.pd() 
+        pen(trtl, 250-(50*i), -250, "lightgrey")        
         trtl.fd(500)
 
-    trtl.pu()
-    trtl.setx(0)
-    trtl.sety(-250)
-    trtl.color("black")
-    trtl.pd() 
+    #Svart x-linje
+    pen(trtl, 0, -250, "black")     
     trtl.fd(500)
 
-    # set position again
-    trtl.penup()
-    trtl.setpos(246,-8)
-    trtl.pendown()
-
+    #Pilen till höger
+    pen(trtl, 246, -8, "black") 
     trtl.write(">", font=("Verdana", 12, "bold"))
 
-    trtl.pu()
-    trtl.setx(-250)
-    trtl.sety(0)
+    #Svart y-linje
+    pen(trtl, -250, 0, "black")
     trtl.rt(90)
-    trtl.pd() 
     trtl.fd(500)
 
-    trtl.penup()
-    trtl.setpos(-6,240)
-    trtl.pendown()
-
+    #Pilen uppåt
+    pen(trtl, -6, 240, "black")
     trtl.write("^", font=("Verdana", 12, "bold"))
      
-    # set position again
-    trtl.penup()
-    trtl.setpos(235,0)
-    trtl.pendown()
-     
-    # write x
+    #Skriver ut "x"
+    pen(trtl, 235, 0, "black")
     trtl.write("x",font=("Verdana", 12, "bold"))
      
-    # set position again
-    trtl.penup()
-    trtl.setpos(5,235)
-    trtl.pendown()
-     
-    # write y
+    #Skriver ut "y"
+    pen(trtl, 5, 235, "black")
     trtl.write("y",font=("Verdana", 12, "bold"))
-
     
-     
-    # set position again
-    trtl.penup()
-    trtl.setpos(5,43)
-    trtl.pendown()
-     
-    # write 1
+    #Skriver ut "1"
+    pen(trtl, 5, 43, "black")
     trtl.write("1",font=("Verdana", 12, "bold"))
 
-    # set position again
-    trtl.penup()
-    trtl.setpos(46,5)
-    trtl.pendown()
-     
-    # write 1
+    #Skriver ut "1"
+    pen(trtl, 46, 5, "black")
     trtl.write("1",font=("Verdana", 12, "bold"))
-
     
     trtl.hideturtle()
 
